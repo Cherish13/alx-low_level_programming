21bc13d8e94cac8cbbdeb9df2d621a8b9b8b9887
Ossai Cherish Nneka <cherishlyte13@gmail.com>
2022-09-09 05:28:36 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
