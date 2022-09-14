1e25678331b0b41896f1cf1b9201cde12e034c49
Ossai Cherish Nneka <cherishlyte13@gmail.com>
2022-09-14 10:32:55 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,8 +10,28 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
-#end
+
+#endif
+^C
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root@8eace494e3c3:~/alx-low_level_programming/0x02-functions_nested_loops# ls
+
