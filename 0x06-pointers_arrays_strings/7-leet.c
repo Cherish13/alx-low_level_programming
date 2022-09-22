d3bee7d53d83ab9ff678dbf72372b37d3d65bbb7
Ossai Cherish Nneka <cherishlyte13@gmail.com>
2022-09-22 14:42:06 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * leet - encodes a string into 1337
- * @s: input string.
- * Return: the pointer to dest.
+ * leet - encodes a string into 1337.
+ * letters a and A are replaced by 4.
+ * Letters e and E are replaced by 3
+ *Letters o and O are replaced by 0
+ *Letters t and T are replaced by 7
+ *Letters l and L are replaced by 1
+ *@s: pointer to string.
+ *Return: pointer to s.
  */
-
 char *leet(char *s)
 {
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
-
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
-			{
-				*(s + count) = numbers[i];
-				break;
-			}
-		}
-		count++;
-	}
-
-	return (s);
+int stringCount, leetCount;
+char leetLetters[] = "aAeEoOtTlL";
+char leetNums[] = "4433007711";
+stringCount = 0;
+while (s[stringCount] != '\0')
+{
+leetCount = 0;
+while (leetCount < 10)
+{
+if (leetLetters[leetCount] == s[stringCount])
+{
+s[stringCount] = leetNums[leetCount];
+}
+leetCount++;
+}
+stringCount++;
+}
+return (s);
 }
