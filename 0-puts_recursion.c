69ba879d9410246de277f0a6573c4c7eebe58568
Ossai Cherish Nneka <cherishlyte13@gmail.com>
2022-09-29 12:20:33 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -1,11 +1,17 @@
 #include "main.h"
-int main()
-{
-    char array[100];
-   
-    printf("Enter a string\n");
-    scanf("%s", array);
 
-    printf("Your string: %s\n", array);
-    return 0;
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
